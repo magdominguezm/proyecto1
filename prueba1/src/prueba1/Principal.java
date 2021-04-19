5260a2497e802daa29424e5759f4c60c330acc10
magdominguezm <magdominguezm@gmail.com>
2021-04-19 21:10:49 +0200

    Cambios en la clase "Principal".

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,6 +3,7 @@ package prueba1;
 public class Principal {
 	public static void main (String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 	}
-
 }
